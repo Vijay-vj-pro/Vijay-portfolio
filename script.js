239762b2cd0c13273cb22e7841348fc3a6c444ef
Vijay <vijaysveera145@gmail.com>
2024-11-14 17:28:20 +0530

    Initial commit (Files uploaded)

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("JavaScript file connected successfully!");
+function showBgImage(){   
+    document.getElementById("vj-img").src = "Images/front-image.jpg";
+}
+function hideBgImage(){
+    document.getElementById("vj-img").src="Images/front-image-removebg1.png";
+}
