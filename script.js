57526054593203ef00e226caec7c72a1805dfe9b
Vijay <vijaysveera145@gmail.com>
2024-11-14 18:39:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("JavaScript file connected successfully!");
 function showBgImage(){   
-    document.getElementById("vj-img").src = "Images/front-image.jpg";
+    document.getElementById("vj-img").src = "Images/front-image1.jpg";
 }
 function hideBgImage(){
     document.getElementById("vj-img").src="Images/front-image-removebg1.png";
