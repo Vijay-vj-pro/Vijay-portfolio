b1957fe257d02f9ca1fb431c6eb6f433a1fd32ab
Vijay <22sucg13@tcarts.in>
2024-11-14 20:08:52 +0530

    images name changed to color and bw

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("JavaScript file connected successfully!");
 function showBgImage(){   
-    document.getElementById("vj-img").src = "Images/front-image-color.jpg";
+    document.getElementById("vj-img").src = "Images/color.jpg";
 }
 function hideBgImage(){
-    document.getElementById("vj-img").src= "Images/front-image-removebg1.png";
+    document.getElementById("vj-img").src= "Images/bw.png";
 }
