24918385fb19f2a3f0e5d86bcc3abb471b772aca
Vijay <22sucg13@tcarts.in>
2024-12-05 21:55:30 +0530

    Some updations

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 console.log("JavaScript file connected successfully!");
+
 function showBgImage(){   
     document.getElementById("vj-img").src = "Images/color.jpg";
 }
 function hideBgImage(){
     document.getElementById("vj-img").src= "Images/bw.png";
+}
+function showHoverBox(hoverBox) {
+    hoverBox.style.display = "flex"; // Make the hover box visible
+}
+function hideHoverBox(hoverBox) {
+    hoverBox.style.display = "none"; // Hide the hover box
+}
+function showProjectName(projectName) {
+    projectName.innerHTML = "View Project 👀";
+}
+function hideProjectName(projectName) {
+    projectName.innerHTML = projectName.getAttribute("data-original-text");
+}
+
+// Show or hide the button based on scroll position
+const scrollUpButton = document.getElementById("scroll-up-btn");
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollUpButton.style.display = "flex"; // Show button
+    } else {
+        scrollUpButton.style.display = "none"; // Hide button
+    }
+};
+function scrollUp() {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+}
+function scrollToHome(){
+    document.getElementById("Home").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToEducation(){
+    document.getElementById("Education").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToAbout(){
+    document.getElementById("About").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToSkills(){
+    document.getElementById("Skills").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToProjects(){
+    document.getElementById("Projects").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToCertifications(){
+    document.getElementById("Certifications").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+function scrollToContact(){
+    document.getElementById("Contact").scrollIntoView({ behavior: "smooth", block: "start" })
 }
